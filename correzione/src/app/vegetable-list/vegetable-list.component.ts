7d6680fd99b8dbe2b25861b1f3fa730f3c0a6176
MatteoSB11 jgjg <matteosbarra07@gmail.com>
2025-05-22 08:11:34 +0000

    Punto 4

--- correzione/src/app/vegetable-list/vegetable-list.component.ts
+++ correzione/src/app/vegetable-list/vegetable-list.component.ts
@@ -9,4 +9,7 @@ import { Vegetables } from '../models/vegetables.model';
 })
 export class VegetableListComponent {
   @Input() verdura!: Vegetables
+  aggiungi(nProdotto: HTMLInputElement){
+    console.log("Nome: " + this.verdura.nome + " Descrizione: " + this.verdura.descrizione + " Prezzo:  " + this.verdura.prezzo + " Numero prodotti: " + nProdotto.value)
+  }
 }
